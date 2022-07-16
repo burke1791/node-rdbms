a7faadd76f0c3d93b25f6b16998f3fa64976b968
Chris Burke <burke1791@gmail.com>
2022-07-16 12:16:12 -0500

    added slot array

--- src/bufferPool/page.js
+++ src/bufferPool/page.js
@@ -1,4 +1,4 @@
-import { PAGE_SIZE } from '../utilities/constants';
+import { EMPTY_SPACE_CHAR, PAGE_SIZE } from '../utilities/constants';
 import { padNumber, padStringTrailing } from '../utilities/helper';
 
 /**
@@ -11,21 +11,7 @@ function Page() {
   this.recordCount = 0;
   this.firstFreeData = 0;
   this.data = '';
-
-  this.deserializeRow = (recordId) => {
-    const recordIndex = recordId * this.recordSize;
-    const recordData = this.data.substring(recordIndex, recordIndex + this.recordSize);
-
-    const id = Number(recordData.substring(4, 8));
-    const age = Number(recordData.substring(8, 12));
-    const name = recordData.substring(12, 62);
-
-    const record = new Record(id, age, name);
-
-    const result = record.deserializeRecord();
-
-    return result;
-  }
+  this.slotArray = ''
 
   this.serializeRow = (personId, age, name) => {
     let nullBitmapOffset = 62;
@@ -35,18 +21,24 @@ function Page() {
       personId = null;
       nullBitmap += '1';
       nullBitmapOffset -= 4;
+    } else {
+      nullBitmap += '0';
     }
 
     if (age == 'null') {
       age = null;
       nullBitmap += '1';
       nullBitmapOffset -= 4;
+    } else {
+      nullBitmap += '0';
     }
 
     if (name.trim() == 'null') {
       name = null;
       nullBitmap += '1';
       nullBitmapOffset -= 50;
+    } else {
+      nullBitmap += '0';
     }
 
     let recordText = '';
@@ -57,27 +49,84 @@ function Page() {
     recordText += name == null ? '' : padStringTrailing(name, 50);
     recordText += nullBitmap;
 
-    this.data = this.data.substring(0, this.firstFreeData) + recordText
+    return recordText;
   }
 
-  this.addRecordToPage = (recordId, recordData) => {
-    const currentRecordIndex = recordId * this.recordSize;
-    const nextRecordIndex = (recordId + 1) * this.recordSize;
+  this.deserializeRow = (recordIndex) => {
+    const nullBitmapOffset = this.data.substring(recordIndex, recordIndex + 4);
+    console.log(nullBitmapOffset);
+    const nullBitmap = this.data.substring(recordIndex + Number(nullBitmapOffset), recordIndex + Number(nullBitmapOffset) + 5);
+    console.log(nullBitmap);
+    const nullBitmapColumns = nullBitmap.substring(2).split('');
 
-    this.data = this.data.substring(0, currentRecordIndex) + recordData + this.data.substring(nextRecordIndex);
+    const columns = [];
+    let colIndex = recordIndex + 4;
+
+    for (let i = 0; i < nullBitmapColumns.length; i++) {
+      if (nullBitmapColumns[i] == '1') return null;
+
+      if (i == 2) {
+        const col = this.data.substring(colIndex, colIndex + 50);
+        console.log(col);
+        columns.push(col);
+        colIndex += 50;
+      } else {
+        const col = this.data.substring(colIndex, colIndex + 4);
+        console.log(col);
+        columns.push(Number(col));
+        colIndex += 4;
+      }
+    }
+
+    let result = '( ';
+
+    columns.forEach(value => {
+      result = result + value + ' ';
+    });
+
+    result = result + ')'
+
+    return result;
+  }
+
+  this.fillInEmptySpace = (recordData) => {
+    let length = recordData.length + this.slotArray.length;
+
+    if (length > PAGE_SIZE) throw new Error('Page cannot exceed ' + PAGE_SIZE + ' chars');
+
+    let text = recordData;
+
+    while (length < PAGE_SIZE) {
+      text = text + EMPTY_SPACE_CHAR;
+      length = text.length + this.slotArray.length;
+    }
+
+    return text;
+  }
+
+  this.addRecordToPage = (newRecordIndex, recordData) => {
+    const allRecordData = this.data.substring(0, newRecordIndex) + recordData;
+    this.firstFreeData = allRecordData.length;
+    this.recordCount++;
+
+    this.slotArray = padNumber(newRecordIndex, 4) + this.slotArray;
+
+    this.data = this.fillInEmptySpace(allRecordData);
   }
 
   this.newRecord = (personId, age, name) => {
-    this.recordData = this.serializeRow(personId, age, name);
+    const rowData = this.serializeRow(personId, age, name);
 
-    this.addRecordToPage(this.nextRecordId, record.data);
-    this.nextRecordId++;
+    this.addRecordToPage(this.firstFreeData, rowData);
   }
 
   this.selectAll = () => {
-    let records = [];
-    for (let i = 0; i < this.recordCount; i++) {
-      records.push(this.deserializeRow(i));
+    const records = [];
+
+    const slotArr = this.slotArray.match(/[\s\S]{1,4}/g) || [];
+    for (let i = slotArr.length - 1; i >= 0; i--) {
+      let recordIndex = Number(slotArr[i]);
+      records.push(this.deserializeRow(recordIndex));
     }
 
     return records;
